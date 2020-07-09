195c953c41269de5515b0852a4b91b4156752957
Mad Mike <fluential@users.noreply.github.com>
2020-07-09 13:55:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
 /*
-Plugin Name:	WooCommerce Reviews.co.uk/Reviews.io Integration
-Plugin URI:     https://github.com/fluential/Reviewscouk-WooCommercePlugin-minifast
-Description:	Built for speed WooCommerce Plugin for Reviews.co.uk
-Text Domain:    rio
-Version:        1.0.2
+Plugin Name:	WooCommerce Reviews.io Integration
+Plugin URI:   https://github.com/fluential/Reviewscouk-WooCommercePlugin-minifast
+Description:	Built for speed WooCommerce Plugin for Reviews.io & Reviews.co.uk
+Text Domain:  rio
+Version:      1.0.2
 Author:		    fluential
 License:	    Apache-2.0
 License URI:	http://www.apache.org/licenses/LICENSE-2.0
